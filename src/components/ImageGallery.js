b272eb473918ec12093eef9b43cd09978750d42e
ecguerra <elizabethcguerra@gmail.com>
2022-06-29 19:29:47 -0400

    basic GET set up

--- src/components/ImageGallery.js
+++ src/components/ImageGallery.js
@@ -1,6 +1,20 @@
+import { useState, useEffect } from 'react'
 import Image from './Image'
+import { getImages } from '../service'
 
 const ImageGallery = () => {
+    const [images, setImages] = useState([])
+
+    useEffect(() => {
+        getImages().then(response => {
+            console.log(response.data.hits)
+        },
+        (error) => {
+            console.log(error)
+        }
+        )
+    }, [])
+
     return(
         <div>
             <h1>HI</h1>
---
+++ src/service.js
@@ -0,0 +1,7 @@
+import axios from 'axios'
+const pixabayURL = 'https://pixabay.com/api/'
+const pixabayKey = process.env.REACT_APP_PIXABAY_API_KEY
+
+export const getImages = () => {
+    return axios.get(`${pixabayURL}?key=${pixabayKey}&q=yellow+flowers&image_type=photo`)
+}
