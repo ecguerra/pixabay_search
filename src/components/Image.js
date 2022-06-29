4a17dd3e36b3386dd371cbc0df9efff4a968559a
ecguerra <elizabethcguerra@gmail.com>
2022-06-29 19:16:27 -0400

    basic setup

---
+++ src/components/Image.js
@@ -0,0 +1,9 @@
+const Image = () => {
+    return (
+        <div>
+            <h2>Image here</h2>
+        </div>
+    )
+}
+
+export default Image
---
+++ src/components/ImageGallery.js
@@ -0,0 +1,12 @@
+import Image from './Image'
+
+const ImageGallery = () => {
+    return(
+        <div>
+            <h1>HI</h1>
+            <Image />
+        </div>
+    )
+}
+
+export default ImageGallery
